45a99db162bd8cb6fbd9151390b55b1ae7fd4c4d
Tomas <tosanchez@frba.utn.edu.ar>
2021-01-30 16:08:51 -0300

    fix: correct timeline parsing

--- src/util/api/engine/TimeLineHandler.js
+++ src/util/api/engine/TimeLineHandler.js
@@ -2,44 +2,69 @@
 const aMonthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const isInCurrentMoth = (sDate, iMonth, iYear) => {
-  return sDate.startsWith(iMonth.toString()) && sDate.endsWith(iYear.toString());
+  return getMonth(sDate) === iMonth && sDate.endsWith(iYear.toString());
 };
 
 const getMonth = (sDate) => {
   return parseInt(sDate.substr(sDate, sDate.indexOf('/')), 10);
 };
 
-const parseTimeLine = async (measure) => {
-  var aDates = Object.getOwnPropertyNames(measure),
-    aTimeLine = [],
-    iYear = new Date().getFullYear() % 100,
-    iLastMonth = getMonth(aDates[aDates.length - 1]);
+/**
+ * Timeline generator
+ * @private
+ * @function
+ * @param {array} aDates the array with all dates
+ * @param {object} oMeasure the object from where to obtain the date data
+ * @param {int} lastMonth the last month number
+ * @return {array} a Timeline array
+ */
+const createTimeline = (aDates, oMeasure, lastMonth) => {
+  var aTimeLine = [],
+    iThisYear = new Date().getFullYear() % 100;
 
-  var i = 0,
-    year = 20;
+  for (var month = 1, year = 20; year <= iThisYear; month++) {
+    // Cut loop when passed the last month
+    if (month === lastMonth + 1 && year === iThisYear) break;
 
-  for (i = 1, year = 20; year <= iYear; i++) {
-    if (i === iLastMonth + 1 && year === iYear) break;
-
-    var aMonths = aDates.filter((sDate) => {
-      return isInCurrentMoth(sDate, i, year);
+    // Getting all dates from the current Month
+    var aDays = aDates.filter((sDate) => {
+      return isInCurrentMoth(sDate, month, year);
     });
 
-    var aMeasures = aMonths.map((sDay) => measure[sDay]);
+    // getting the date measures
+    var aMeasures = aDays.map((sDay) => oMeasure[sDay]);
 
+    // total measures from the month
     var iMeasure = aMeasures.reduce((totalMeasures, measures) => {
       return totalMeasures + measures;
     }, 0);
 
-    aTimeLine.push({ month: aMonthNames[i - 1], measure: iMeasure, year: year });
+    // adding to timeline
+    aTimeLine.push({ month: aMonthNames[month - 1], measure: iMeasure, year: year });
 
-    if (i === 12) {
+    // updating year
+    if (month === 12) {
       year++;
-      i = 1;
+      month = 0;
     }
   }
 
   return aTimeLine;
 };
 
-export default parseTimeLine;
+const createHistoricalData = async (oHistorical) => {
+  var oHistoricalData = {};
+
+  var aMeasures = ['cases', 'deaths', 'recovered'];
+
+  aMeasures.forEach((sMeasure) => {
+    var aDates = Object.getOwnPropertyNames(oHistorical.timeline[sMeasure]),
+      iLastMonth = getMonth(aDates[aDates.length - 1]);
+
+    var aTimeLine = createTimeline(aDates, oHistorical.timeline[sMeasure], iLastMonth);
+  });
+
+  return oHistoricalData;
+};
+
+export default createHistoricalData;
