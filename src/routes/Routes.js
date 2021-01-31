a89c564789a829479a74fceb65077cca2d4705b7
Tomas <tosanchez@frba.utn.edu.ar>
2021-01-31 14:00:11 -0300

    build: delete unused elements

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,10 +3,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { getUrl } from '../util/browser/BrowserProvider';
 
 import { BusyIndicator } from '@ui5/webcomponents-react/lib/BusyIndicator';
-import RouteWithAuthorizationRestriction from '../auth/RouteWithAuthorizationRestriction';
 
-const TodoList = lazy(() => import('../pages/Todo/List/TodoList'));
-const TodoEdit = lazy(() => import('../pages/Todo/Edit/TodoEdit'));
 const NotFound = lazy(() => import('../pages/Fallback/NotFound'));
 const Buggy = lazy(() => import('../pages/Fallback/Buggy'));
 const Dashboard = lazy(() => import('../pages/Overview/Dashboard/Dashboard'));
@@ -17,8 +14,6 @@ const Routes = () => {
     <Suspense fallback={<BusyIndicator active />}>
       <Switch>
         <Redirect path={getUrl('HOME')} exact to={getUrl('OVERVIEW')} />
-        <RouteWithAuthorizationRestriction allowedAuthorities={['canAccessTodoListPage']} authorityKey="permissions" path={getUrl('TODO_LIST')} component={TodoList} />
-        <RouteWithAuthorizationRestriction allowedAuthorities={['canAccessTodoEditPage']} authorityKey="permissions" path={getUrl('TODO_EDIT')} component={TodoEdit} />
         <Route path={getUrl('BUGGY')} exact component={Buggy} />
         <Route path={getUrl('OVERVIEW')} exact component={Dashboard} />
         <Route path={getUrl('DETAILS')} exact component={Details} />
